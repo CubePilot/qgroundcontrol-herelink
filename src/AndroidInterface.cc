29bc07d0750d3d55a82d5163ab953d3e5ff34657
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2020-02-11 10:50:21 +0530

    add gstreamer

---
+++ src/AndroidInterface.cc
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *
+ * Copyright (C) 2018 Pinecone Inc. All rights reserved.
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#include <QtAndroidExtras/QtAndroidExtras>
+#include <QtAndroidExtras/QAndroidJniObject>
+#include "QGCApplication.h"
+#include "AndroidInterface.h"
+#include <QAndroidJniObject>
+
+QString AndroidInterface::getSdcardPath()
+{
+    QAndroidJniObject value = QAndroidJniObject::callStaticObjectMethod("org/mavlink/qgroundcontrol/QGCActivity", "getSdcardPath",
+                            "()Ljava/lang/String;");
+    return value.toString();
+}
+
+void AndroidInterface::triggerMediaScannerScanFile(QString& file_path)
+{
+    QAndroidJniObject path = QAndroidJniObject::fromString(file_path);
+     QAndroidJniObject::callStaticObjectMethod("org/mavlink/qgroundcontrol/QGCActivity", "triggerMediaScannerScanFile",
+                                               "(Ljava/lang/String;)V",
+                                               path.object<jstring>());
+}
